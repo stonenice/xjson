b7d240b89010a6214a0a955c486505ba0578d359
stone <stonenice@outlook.com>
2017-12-02 22:24:29 +0800

    Create NamesType.java

---
+++ src/main/java/com/xasync/xjson/NamesType.java
@@ -0,0 +1,25 @@
+package com.xasync.iplain;
+
+/**
+ * NamesType defined which rules are used to name your class, property and so on.
+ * the most of rules are listed in this class. if you followed the rules, so you
+ * can make some translation each rules.
+ *
+ * Author: stonenice@outlook.com
+ * Date: 2016-12-28
+ */
+public enum NamesType {
+    UNKNOWN,
+
+    UNDERLINE_LOWER_CASE,
+
+    UNDERLINE_UPPER_CASE,
+
+    CAMEL_LOWER_CASE,
+
+    CAMEL_UPPER_CASE,
+
+    ALL_LOWER_CASE,
+
+    ALL_UPPER_CASE
+}
